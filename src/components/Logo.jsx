239e02fd957c00134de8eace90fd8b0baf230814
Rajat Chauhan <rajatchauhan9690@gmail.com>
2024-06-06 04:40:52 +0530

    update vite.config.js

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,17 +3,14 @@ import { IoLogoYoutube } from "react-icons/io";
 import { Link } from "react-router-dom";
 
 function Logo({ size = "30" }) {
-    return (
-        <>
-            <Link to={'/'} className="flex gap-2 items-center">
-                <IoLogoYoutube
-                    size={size}
-                    color="#A855F7"
-                />
-                <span className="font-bold text-white">YOUTUBE</span>
-            </Link>
-        </>
-    );
+  return (
+    <>
+      <Link to={"/"} className="flex gap-2 items-center">
+        <IoLogoYoutube size={size} color="#A855F7" />
+        <span className="font-bold text-white">YOUTUBE</span>
+      </Link>
+    </>
+  );
 }
 
 export default Logo;
